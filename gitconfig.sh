c02ba1a7fb2a012e547d07d57cabeb174521f1d8
Liew Jun Tung <pandawarrior91@gmail.com>
2015-08-04 01:47:50 +0800

    first commit

---
+++ gitconfig.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+git config --global alias.lg "log --oneline --decorate --all --graph"
+git config --global alias.s "status -s"
+git config --global pull.rebase true
+git config --global rerere.enabled true
+git config --global user.name "Liew Jun Tung"
+git config --global user.email pandawarrior91@gmail.com
+
